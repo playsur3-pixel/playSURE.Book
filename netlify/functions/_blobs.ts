7b3501d445cb6522f8fb62d7d723bd6cbec8a00a
playSURE <playsur3@gmail.com>
2026-02-26 06:52:16 +0100

    Supr netlify function

--- netlify/functions/_blobs.ts
+++
@@ -1,8 +0,0 @@
-import type { HandlerEvent } from "@netlify/functions";
-import { connectLambda, getStore } from "@netlify/blobs";
-
-export function getBlobsStore(event: HandlerEvent, name: string) {
-  // obligatoire en "lambda compatibility mode"
-  connectLambda(event as any);
-  return getStore(name);
-}
